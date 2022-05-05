b6adc3e17892fb3af2ecc34e1ea7407d97175e39
Pepa Pánek <josef.panek@gmail.com>
2022-05-05 15:49:41 +0200

    Add project files.

---
+++ HelloObjectWorld/Zdravic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloObjectWorld
+{
+    /// <summary>
+    /// Třída reprezentuje zdravič, který slouží ke zdravení uživatelů
+    /// </summary>
+    class Zdravic
+    {
+        /// <summary>
+        /// Text pozdravu
+        /// </summary>
+        public string text;
+        /// <summary>
+        /// Pozdraví uživatele textem pozdravu a jeho jménem
+        /// </summary>
+        /// <param name="jmeno">Jméno uživatele</param>
+        /// <returns>Text s pozdravem</returns>
+        public string Pozdrav(string jmeno)
+        {
+            return String.Format("{0} {1}!", text, jmeno);
+        }
+    }
+}
